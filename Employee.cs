833617d166edf0d14b709b8e976aabdf1fc3751f
Brennen Tatum <bmar566@gmail.com>
2020-05-28 17:10:39 -0500

    completed classes exercise

---
+++ Employee.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace classes
+{
+    public class Employee
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime StartDate;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Company AAA = new Company()
+            {
+                Name = "AAA",
+            };
+
+            Employee Joe = new Employee()
+            {
+                Firstname = "Joe",
+                Lastname = "Blo",
+                Title = "Programmer"
+            };
+            AAA.employees.Add(Joe);
+
+            Employee John = new Employee()
+            {
+                Firstname = "John",
+                Lastname = "Doe",
+                Title = "Developer"
+            };
+            AAA.employees.Add(John);
+
+            Employee Jane = new Employee()
+            {
+                Firstname = "Jane",
+                Lastname = "Doe",
+                Title = "Programmer1"
+            };
+            AAA.employees.Add(Jane);
+
+            foreach (Employee emp in AAA.employees)
+            {
+                Console.WriteLine($"{emp.Firstname} {emp.Lastname} is a {emp.Title}");
+            }
+
+            // Iterate the company's employee list and generate the
+            // simple report shown above
+
+        }
+    }
+}
